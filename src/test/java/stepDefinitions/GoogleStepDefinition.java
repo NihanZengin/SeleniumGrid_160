a66aee203fab3d996c8c7168de2ec9bbda2c84fc
NihanZengin <nihan.zengin90@gmail.com>
2025-05-06 10:27:07 +0200

    SeleniumGrid

---
+++ src/test/java/stepDefinitions/GoogleStepDefinition.java
@@ -0,0 +1,27 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+public class GoogleStepDefinition {
+    WebDriver driver = Hooks.driver;
+
+    @Given("Google ana sayfasina git")
+    public void googleAnaSayfasinaGit() {
+        driver.get("https://www.google.com");
+    }
+
+    @Then("Basligin {string} oldugunu dogrula")
+    public void basliginOldugunuDogrula(String expectedTitle) {
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+    }
+
+    @Then("Sayfayi kapat")
+    public void sayfayiKapat() {
+        //	DriverFactory.quitDriver();
+    }
+
+}
